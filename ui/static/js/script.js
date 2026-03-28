287b57e6b2b90faddc956cb29e4ba6609451bf6a
= <=>
2026-03-28 18:57:50 -0400

    Update file

--- ui/static/js/script.js
+++ ui/static/js/script.js
@@ -10,6 +10,15 @@ SubmitButton.addEventListener('click', () => {
 ConfirmButton.addEventListener('click', () => {
     myDialog.close();
     // You can add code here to handle the form submission
+    form_info.firstname = document.getElementById('firstName').value;
+    form_info.lastname = document.getElementById('lastName').value;
+    form_info.email = document.getElementById('email').value;
+    form_info.DOB = document.getElementById('dob').value;
+    form_info.insurance = document.getElementById('insurance').value;
+    form_info.address = document.getElementById('address').value;
+    form_info.medical_department = document.getElementById('medical_department').value;
+    form_info.medical_history = document.getElementById('additionalInfo').value;
+    console.log(form_info);
 
 });
 
