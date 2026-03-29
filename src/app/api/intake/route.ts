f2cbe9ea4f93ad4fcc8ef0118bcc7d70f83630a2
Amodh Dhakal <amodhakal@gmail.com>
2026-03-29 02:58:58 -0400

    fix: stiringify hosptial mock response

--- src/app/api/intake/route.ts
+++ src/app/api/intake/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: NextRequest) {
       body: JSON.stringify({
         email: translatedData.email,
         language: sourceLanguage,
-        info: mockHospitalResponse,
+        info: JSON.stringify(mockHospitalResponse),
       }),
     });
 
